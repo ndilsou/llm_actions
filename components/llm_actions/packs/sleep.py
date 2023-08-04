5e52190e71462bbb1a439df9f89547b8f1578a7b
Ndil-Adjim  Sou Ngadoy <nasoungadoy@gmail.com>
2023-08-04 17:25:07 +0000

    add a simple sleeping skill

---
+++ components/llm_actions/packs/sleep.py
@@ -0,0 +1,34 @@
+"""Sleeping action pack. The pack contains simple actions that allow the agent to sleep for a specified amount of time."""
+
+import asyncio
+from datetime import timedelta
+from time import sleep
+from pydantic import Field
+
+from llm_actions.base import Result, ok, ActionModel
+
+
+class Sleep(ActionModel[str, None]):
+    """Pauses the program and sleep for a specified number of seconds."""
+
+    duration: int | timedelta = Field(..., description="Time to sleep in seconds")
+
+    def _do(self) -> Result[str, None]:
+        duration: int
+        if isinstance(self.duration, timedelta):
+            duration = int(self.duration.total_seconds())
+        else:
+            duration = self.duration
+
+        sleep(duration)
+        return ok(f"Slept for {duration} seconds")
+    
+    async def _ado(self) -> Result[str, None]:
+        duration: int
+        if isinstance(self.duration, timedelta):
+            duration = int(self.duration.total_seconds())
+        else:
+            duration = self.duration
+
+        await asyncio.sleep(duration)
+        return ok(f"Slept for {duration} seconds")
